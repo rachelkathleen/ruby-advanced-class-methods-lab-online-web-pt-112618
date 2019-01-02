771e26254f81e033ae81d4e97aa5c9652ed5ceeb
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-02 20:28:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,17 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
-  end
-
-  def self.create(name, artist_name)
-    song.new(name, artist_name)
-    self
   end
 
 end
