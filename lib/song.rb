f121a4589e66909808c9321fa7cfbc7ebc7367fd
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-02 21:18:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) == song_name ? self.find_by_name(song_name) : self.create_by_name(song_name)
+    self.find_by_name(song_name) == song_name ? song_name : self.create_by_name(song_name)
+    song_name
   end
 
 end
