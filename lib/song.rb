c5b1a4cfba50b87877750be277c99fc0eabb22ba
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-02 21:29:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     @@all.sort_by{|song| song.name}
   end
 
-  def self.create_from_filename(filename)
+  def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
